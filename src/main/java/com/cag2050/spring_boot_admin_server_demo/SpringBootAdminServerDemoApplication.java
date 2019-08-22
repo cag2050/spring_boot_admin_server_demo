70c20e5d54f251d9dcb4cd06b937ac34f13bd16a
chen_anguo <cag2050@163.com>
2019-08-22 10:46:30 +0800

    project init

---
+++ src/main/java/com/cag2050/spring_boot_admin_server_demo/SpringBootAdminServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cag2050.spring_boot_admin_server_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminServerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cag2050/spring_boot_admin_server_demo/SpringBootAdminServerDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cag2050.spring_boot_admin_server_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAdminServerDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
